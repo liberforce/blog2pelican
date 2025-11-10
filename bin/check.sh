23dff5a606330d1b2eee3ec6de3765881b0e54c9
Luis Menina <liberforce@freeside.fr>
2025-11-10 12:12:48 +0100

    Add helper to call all linters and fixers

---
+++ bin/check.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+this_script=$(readlink -e "$0")
+this_script_dir=$(realpath $(dirname "${this_script}"))
+
+toplevel_dir=$(realpath "${this_script_dir}/..")
+
+project="blog2pelican"
+mypy "${toplevel_dir}/${project}"
+ruff check "${toplevel_dir}/${project}"
+black "${toplevel_dir}/${project}"
