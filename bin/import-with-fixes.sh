b853f9466c4ba52f3387a890f207fb5a8b17f073
Luis Menina <liberforce@freeside.fr>
2025-11-10 06:38:37 +0100

    Refactor: move to a package

--- bin/import-with-fixes.sh
+++ bin/import-with-fixes.sh
@@ -6,4 +6,4 @@ this_script_dir=$(realpath $(dirname "${this_script}"))
 
 export PATH="${PATH}:${this_script_dir}:${this_script_dir}/.."
 
-blog2pelican.py --dotclear --markup markdown "$@"
+python -m blog2pelican.tool --dotclear --markup markdown "$@"
