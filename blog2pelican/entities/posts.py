25dd38be659ab8a40d0222d089082bedde5e5fa2
Luis Menina <liberforce@freeside.fr>
2025-11-11 04:25:10 +0100

    iRefactor: extract PelicanPost entity class

---
+++ blog2pelican/entities/posts.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class PelicanPost:
+    title: str
+    content: str
+    filename: str
+    date: str
+    author: str
+    categories: list[str]
+    tags: list[str]
+    status: str
+    kind: str
+    in_markup: str
