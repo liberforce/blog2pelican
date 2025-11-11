2dc5a06f0e2f0e91b8a7f4f96959f341d6ddce79
Luis Menina <liberforce@freeside.fr>
2025-11-11 04:53:12 +0100

    Add dotclear test

---
+++ tests/test_dotclear.py
@@ -0,0 +1,22 @@
+from blog2pelican.parsers.dotclear import DotclearParser
+from blog2pelican.entities.posts import PelicanPost
+
+
+def test_simple():
+    parser = DotclearParser()
+    posts = parser.parse("tests/data/dotclear/simple.txt")
+    actual = PelicanPost(*next(posts))
+    expected = PelicanPost(
+        title="En direct d'Istanbul",
+        content="<p>first paragraph</p>\\r<p>second paragraph</p>",
+        filename="en-direct-distanbul",
+        date="2008-07-07 08:08",
+        author="TEST-GANDI",
+        categories=["Life"],
+        tags=["GUADEC", "GNOME"],
+        status="published",
+        kind="article",
+        in_markup="html",
+    )
+
+    assert actual == expected
